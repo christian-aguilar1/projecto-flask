75f24efed722d4184861c3ef5bf6ac8f1fcf0f9c
Christian Aguilar <christianaguilarm1@gmail.com>
2021-05-04 19:01:02 -0400

    navbar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
+bootstrap = Bootstrap(app)
 
 todos = ['Buy coffee', 'Send purchase request', 'Deliver video to producer']
 
