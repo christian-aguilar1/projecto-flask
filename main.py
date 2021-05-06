1ecea53d3a15e84309649f55910635ad6926b8cf
Christian Aguilar <christianaguilarm1@gmail.com>
2021-05-06 09:47:12 -0400

    advance

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Buy coffee', 'Send purchase request', 'Deliver video to producer']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -17,23 +29,30 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
-# @app.route('/')
-# def index():
-#     raise(Exception('500 error'))
-
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
 
-    return render_template('hello.html.j2', **context)
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        return redirect(url_for('index'))
+
+    return render_template('hello.j2.html', **context)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
